065411f7979061f59f3bac5cd3c329f966f95cbf
Juan Pablo <shunikv@gmail.com>
2025-05-25 20:18:31 -0600

    cors habilitados

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3002);
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`App running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
